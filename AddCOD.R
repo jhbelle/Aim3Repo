fbc40a7e286cb0df9681515766778e2358e1e212
Belle <jhbelle@emory.edu>
2018-02-01 10:29:12 -0500

    Added script to add COD from Interpolated COD files into combined files

---
+++ AddCOD.R
@@ -0,0 +1,52 @@
+## -----------
+## Name: AddCOD.R
+## Program version: R 3.3.3
+## Dependencies:
+## Author: J.H. Belle
+## Purpose: Add Interpolated Cloud AOD values to Combined files containing other variables
+## -----------
+
+# Load libraries
+
+## ----
+# Define parameters
+## ----
+
+# TA flag
+TAflag = "A"
+
+# Time 
+Startdate = as.Date("2003-01-01", "%Y-%m-%d")
+Enddate = as.Date("2005-12-31", "%Y-%m-%d")
+SeqDates = seq(Startdate, Enddate, by="day")
+
+# Location of Combined files
+CombLoc = "/terra/CombinedValues_Jess_GA_Aqua/"
+
+# Location of interpolated COD files
+InterpCODLoc = "/aura/Jess_MOYD06_MOYD03_Georgia/InterpCOD_Aqua/"
+
+## ----
+# Loop over days
+## ----
+
+for (day in seq_along(SeqDates)){
+  # Extract time variables from date
+  Date = SeqDates[day]
+  Year = as.numeric(as.character(Date, "%Y"))
+  Jday = as.numeric(as.character(Date, "%j"))
+  Combfile = sprintf("%sCombMAIACCloudNLDASGC_Text_%d%03d_%s.csv", CombLoc, Year, Jday, TAflag)
+  # Read Combined file, if it exists
+  CombDat = try(read.csv(Combfile, stringsAsFactors = F))
+  if (is.data.frame(CombDat)){
+    InterpCODDat = try(read.csv(sprintf("%sInterpolatedCOD_%d_%03d.csv"), stringsAsFactors = F))
+    if (is.data.frame(InterpCODDat)){
+      colnames(InterpCODDat) <- c("Input_FID", "x", "y", "InterpCOD")
+      InterpCOD = aggregate(InterpCOD~Input_FID, InterpCODdat)
+      CombDat = merge(CombDat, InterpCOD, by="Input_FID", all.x=T)
+    } else {
+      CombDat$InterpCOD = rep(NA, nrow(CombDat))
+    }
+    write.csv(CombDat, Combfile, row.names = F)
+  }
+}
